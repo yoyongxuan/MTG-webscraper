c5a64d6956ecd9d6b7137b8785f57670c8ebb610
yoyongxuan <66002397+yoyongxuan@users.noreply.github.com>
2022-12-13 14:48:46 +0000

    imported beautifulsoup

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import urllib
+
+url = 'https://www.greyogregames.com/search?q=*rhystic+study*'
+
+page = urlopen(url)
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+result = soup.find_all(class_="buyWrapper")
+print(result)
+
+pattern = '<div class="buyWrapper">'
+
 
