fe35b1df847c56e6b7c56ed30893a62174d0edcb
yoyongxuan <66002397+yoyongxuan@users.noreply.github.com>
2022-12-15 09:32:24 +0000

    Working scraper for greyogregames

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import urllib
+import re
 
-url = 'https://www.greyogregames.com/search?q=*rhystic+study*'
+url = 'https://www.greyogregames.com/search?q=*cultivate*'
 
 page = urlopen(url)
 html = page.read().decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
-result = soup.find_all(class_="buyWrapper")
-print(result)
+result = soup.find_all("div",class_="product Norm")
+for product in result:
+    for card in product.find_all("div", class_= "addNow single"):
+        onclick = card['onclick']
+        addToCart = onclick[10:-1].split(',')
+        name = addToCart[1]
+        print(name)
 
-pattern = '<div class="buyWrapper">'
+        price = int(re.sub(r'[^0-9]', '', card.p.get_text()))
+        print(price)
+
+        print('\n\n')
+    
+
 
 
