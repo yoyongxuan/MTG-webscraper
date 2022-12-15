c0ae6e7dba05a77648ded78717e6c993a69be936
yoyongxuan <66002397+yoyongxuan@users.noreply.github.com>
2022-12-15 11:02:24 +0000

    added function to check for multiple pages

--- main.py
+++ main.py
@@ -3,24 +3,40 @@ from urllib.request import urlopen
 import urllib
 import re
 
-url = 'https://www.greyogregames.com/search?q=*cultivate*'
 
-page = urlopen(url)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
-result = soup.find_all("div",class_="product Norm")
-for product in result:
-    for card in product.find_all("div", class_= "addNow single"):
-        onclick = card['onclick']
-        addToCart = onclick[10:-1].split(',')
-        name = addToCart[1]
-        print(name)
 
-        price = int(re.sub(r'[^0-9]', '', card.p.get_text()))
-        print(price)
+#url = 'https://www.greyogregames.com/search?q=*rhystic+study*'
 
-        print('\n\n')
+url = 'https://www.greyogregames.com/collections/mtg-singles-all-products'
+
+
+def greyogregames_scraper(url,nextpage=False):
+    greyogregames_cardlist = []
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    product_list = soup.find_all("div",class_="product Norm")
+    for product in product_list:
+        for card in product.find_all("div", class_= "addNow single"):
+            onclick = card['onclick']
+            addToCart = onclick[10:-1].split(',')
+            name = addToCart[1]
+            
     
+            price = int(re.sub(r'[^0-9]', '', card.p.get_text()))
+    
+            greyogregames_cardlist.append([name,price])
 
+    if not nextpage:
+        pagination = soup.find_all("div", id="pagination")
+        if len(pagination) > 0:
+            print(True)
+
+        # print(name)
+        # print(price)
+        # print('\n\n')
+    return greyogregames_cardlist
+    
+print(greyogregames_scraper(url))
 
 
