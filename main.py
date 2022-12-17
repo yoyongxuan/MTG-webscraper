7072339325dd141aa63717aefdcd80fee300b0b8
yoyongxuan <66002397+yoyongxuan@users.noreply.github.com>
2022-12-17 08:57:01 +0000

    Changed function to be iterative instead of recursive

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ import csv
 
 
 #url = 'https://www.greyogregames.com/search?q=*rhystic+study*'
-url = 'https://www.greyogregames.com/search?q=*cultivate*'
-#url = 'https://www.greyogregames.com/collections/mtg-singles-all-products'
+#url = 'https://www.greyogregames.com/search?q=*cultivate*'
+url = 'https://www.greyogregames.com/collections/mtg-singles-all-products'
 
-
-def greyogregames_scraper(url):
+def greyogregames_scrape_page(url):
     print(url)
-    greyogregames_cardlist = []
+    cardlist = []
     page = urlopen(url)
     html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
@@ -32,38 +31,41 @@ def greyogregames_scraper(url):
             addToCart = onclick[10:-1].split(',')
             name = addToCart[1]
             price = int(re.sub(r'[^0-9]', '', card.p.get_text()))
-            greyogregames_cardlist.append([name,price])
+            cardlist.append([name,price])
             
         if len(available_cards) == 0:
             name = product.find(class_="productTitle").get_text().strip()
-            greyogregames_cardlist.append([name,"Sold Out"])
+            cardlist.append([name,"Sold Out"])
             
             
-    
-            
-    
-            
-
     next_page = soup.find_all("a",class_="pagination-item pagination-next")
     if len(next_page) > 0:
         next_page_url = 'https://www.greyogregames.com' + next_page[0]['href']
-        greyogregames_cardlist.extend(greyogregames_scraper(next_page_url))
-        
+    else:
+        next_page_url = None      
         # print(name)
         # print(price)
         # print('\n\n')
+    return cardlist,next_page_url
+
+
+def greyogregames_scraper(url):
+    greyogregames_cardlist = []
+    while url != None:
+        cardlist,url = greyogregames_scrape_page(url)
+        greyogregames_cardlist.extend(cardlist)
+
     return greyogregames_cardlist
 
 
 greyogregames_cardlist = greyogregames_scraper(url)
 
 def write_to_file(filename,nestedlist):
-    with open(filename,'w') as f:
+    with open(filename,'w',newline='') as f:
         csv_writer = csv.writer(f)
         for listing in nestedlist:
             csv_writer.writerow(listing)
+            print(listing)
 
 write_to_file("greyogregames_cardlist.csv",greyogregames_cardlist)
 print(greyogregames_cardlist)
-
-
